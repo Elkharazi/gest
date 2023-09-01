abcbed1bc44e782fc3f994739d50c1003e7b70f6
Elkharazi <elkharrazi.d130297236@edu.uiz.ac.ma>
2023-09-01 19:49:03 +0100

    firts project

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,192 @@
+<header class="main-header header-style-one">
+    <div class="header-lower">
+        <div class="logo-box">
+            <figure class="logo"><a href="index-2.html"><img src="assets/images/logo.png" alt=""></a></figure>
+        </div>
+        <div class="outer-box">
+            <div class="menu-area">
+                <!--Mobile Navigation Toggler-->
+                <div class="mobile-nav-toggler">
+                    <i class="icon-bar"></i>
+                    <i class="icon-bar"></i>
+                    <i class="icon-bar"></i>
+                </div>
+                <nav class="main-menu navbar-expand-md navbar-light">
+                    <div class="collapse navbar-collapse show clearfix" id="navbarSupportedContent">
+                        <ul class="navigation clearfix">
+                            <li class="dropdown"><a href="index-2.html">Home</a>
+                                <ul>
+                                    <li><a href="index-2.html">Home Page One</a></li>
+                                    <li><a href="index-3.html">Home Page Two</a></li>
+                                    <li><a href="index-onepage.html">Home OnePage</a></li>
+                                    <li><a href="index-rtl.html">Home RTL</a></li>
+                                    <li class="dropdown"><a href="index-2.html">Header Style</a>
+                                        <ul>
+                                            <li><a href="index-2.html">Header Style One</a></li>
+                                            <li><a href="index-3.html">Header Style Two</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="dropdown"><a href="index-2.html">Activities</a>
+                                <ul>
+                                    <li><a href="activities.html">Activities</a></li>
+                                    <li><a href="climbing.html">Climbing</a></li>
+                                    <li><a href="adventure.html">Adventure</a></li>
+                                    <li><a href="camping.html">Camping</a></li>
+                                    <li><a href="diving.html">Diving</a></li>
+                                    <li><a href="parachute.html">Parachute</a></li>
+                                    <li><a href="throwing.html">Throwing</a></li>
+                                </ul>
+                            </li>
+                            <li class="current dropdown"><a href="index-2.html">Pages</a>
+                                <ul>
+                                    <li><a href="about.html">About Us</a></li>
+                                    <li><a href="team.html">Our Team</a></li>
+                                    <li><a href="gallery.html">Our Gallery</a></li>
+                                    <li><a href="error.html">404</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown"><a href="index-2.html">Elements</a>
+                                <div class="megamenu">
+                                    <div class="row clearfix">
+                                        <div class="col-lg-6 column">
+                                            <ul>
+                                                <li>
+                                                    <h4>Elements 1</h4>
+                                                </li>
+                                                <li><a href="about-element-1.html">About Block 01</a></li>
+                                                <li><a href="about-element-2.html">About Block 02</a></li>
+                                                <li><a href="about-element-3.html">About Block 03</a></li>
+                                                <li><a href="activities-element-1.html">Activities Block 01</a></li>
+                                                <li><a href="activities-element-2.html">Activities Block 02</a></li>
+                                                <li><a href="news-element-1.html">News Block 01</a></li>
+                                                <li><a href="news-element-2.html">News Block 02</a></li>
+                                                <li><a href="team-element-1.html">Team Block 01</a></li>
+                                            </ul>
+                                        </div>
+                                        <div class="col-lg-6 column">
+                                            <ul>
+                                                <li>
+                                                    <h4>Elements 2</h4>
+                                                </li>
+                                                <li><a href="team-element-2.html">Team Block 02</a></li>
+                                                <li><a href="feature-element-1.html">Feature Block 01</a></li>
+                                                <li><a href="feature-element-2.html">Feature Block 02</a></li>
+                                                <li><a href="video-element.html">Video Block</a></li>
+                                                <li><a href="gallery-element.html">Gallery Block</a></li>
+                                                <li><a href="testimonial-element.html">Testimonial Block</a></li>
+                                                <li><a href="cta-element.html">Cta Block</a></li>
+                                                <li><a href="clients-element.html">Clients Block</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="dropdown"><a href="index-2.html">Blog</a>
+                                <ul>
+                                    <li><a href="blog.html">Our Blog</a></li>
+                                    <li><a href="blog-details.html">Blog Details</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="contact.html">Contact</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+            <div class="menu-right-content">
+                <div class="support-box">
+                    <div class="icon"><i class="flaticon-chat"></i></div>
+                    <span>Call Anytime</span>
+                    <h5><a href="tel:12463330079">+1- ( 246 ) 333 - 0079</a></h5>
+                </div>
+                <div class="search-box-outer">
+                    <div class="dropdown">
+                        <button class="search-box-btn" type="button" id="dropdownMenu3" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false"><i
+                                class="flaticon-magnifying-glass"></i></button>
+                        <div class="dropdown-menu search-panel" aria-labelledby="dropdownMenu3">
+                            <div class="form-container">
+                                <form method="post" action="https://azim.commonsupport.com/Amping/blog.html">
+                                    <div class="form-group">
+                                        <input type="search" name="search-field" value=""
+                                            placeholder="Search...." required="">
+                                        <button type="submit" class="search-btn"><span
+                                                class="fas fa-search"></span></button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="cart-box"><a href="index-2.html"><i class="flaticon-shopping-cart"></i></a></div>
+            </div>
+        </div>
+    </div>
+
+    <!--sticky Header-->
+    <div class="sticky-header">
+        <div class="outer-box">
+            <div class="menu-area">
+                <nav class="main-menu clearfix">
+                    <!--Keep This Empty / Menu will come through Javascript-->
+                </nav>
+            </div>
+            <div class="menu-right-content">
+                <div class="support-box">
+                    <div class="icon"><i class="flaticon-chat"></i></div>
+                    <span>Call Anytime</span>
+                    <h5><a href="tel:12463330079">+1- ( 246 ) 333 - 0079</a></h5>
+                </div>
+                <div class="search-box-outer">
+                    <div class="dropdown">
+                        <button class="search-box-btn" type="button" id="dropdownMenu4" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false"><i
+                                class="flaticon-magnifying-glass"></i></button>
+                        <div class="dropdown-menu search-panel" aria-labelledby="dropdownMenu4">
+                            <div class="form-container">
+                                <form method="post" action="https://azim.commonsupport.com/Amping/blog.html">
+                                    <div class="form-group">
+                                        <input type="search" name="search-field" value=""
+                                            placeholder="Search...." required="">
+                                        <button type="submit" class="search-btn"><span
+                                                class="fas fa-search"></span></button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="cart-box"><a href="index-2.html"><i class="flaticon-shopping-cart"></i></a></div>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- Mobile Menu  -->
+<div class="mobile-menu">
+    <div class="menu-backdrop"></div>
+    <div class="close-btn"><i class="fas fa-times"></i></div>
+
+    <nav class="menu-box">
+        <div class="nav-logo"><a href="index-2.html"><img src="assets/images/logo-2.png" alt=""
+                    title=""></a></div>
+        <div class="menu-outer"><!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header--></div>
+        <div class="contact-info">
+            <h4>Contact Info</h4>
+            <ul>
+                <li>Chicago 12, Melborne City, USA</li>
+                <li><a href="tel:+8801682648101">+88 01682648101</a></li>
+                <li><a href="mailto:info@example.com">info@example.com</a></li>
+            </ul>
+        </div>
+        <div class="social-links">
+            <ul class="clearfix">
+                <li><a href="index-2.html"><span class="fab fa-twitter"></span></a></li>
+                <li><a href="index-2.html"><span class="fab fa-facebook-square"></span></a></li>
+                <li><a href="index-2.html"><span class="fab fa-pinterest-p"></span></a></li>
+                <li><a href="index-2.html"><span class="fab fa-instagram"></span></a></li>
+                <li><a href="index-2.html"><span class="fab fa-youtube"></span></a></li>
+            </ul>
+        </div>
+    </nav>
+</div><!-- End Mobile Menu -->
